b4d8f55598035f6484f850e34a96ad5e6c22e340
Fabrizzio20k <fabrizzio.vilchez.e@utec.edu.pe>
2023-12-20 22:13:22 -0500

    Delete uncompatible library

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,11 +1,11 @@
 import styles from "@/styles/Presentation.module.css";
-
 function Presentation() {
+
     return (
         <div className={styles.mainPresentation}>
             <div className={styles.column1}>
                 <h3>Hi, I&apos;m Fabrizzio Vilchez, just a passionated fullstack developer from Lima, Perú 🇵🇪 </h3>
-            </div>
+                T</div>
             <div className={styles.column2}>
                 <p>Espacio para imagen</p>
             </div>
