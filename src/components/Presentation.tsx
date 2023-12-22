91befe1276e58e49a7c6d813a360a1bad45df550
Fabrizzio20k <fabrizzio.vilchez.e@utec.edu.pe>
2023-12-21 21:12:31 -0500

    Add typewritter effect

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,16 +1,36 @@
+"use client";
+
 import styles from "@/styles/Presentation.module.css";
-import Image from "next/image";
 import Link from "next/link";
 import { FaInstagram } from "react-icons/fa";
 import { AiOutlineLinkedin } from "react-icons/ai";
 import { FiGithub } from "react-icons/fi";
+import Typewriter from 'typewriter-effect';
 function Presentation() {
 
     return (
         <div className={styles.mainPresentation}>
             <div className={styles.columnsPresentation}>
                 <div className={styles.column1}>
-                    <h1 className={styles.textPresentation}>Frontend / Backend developer here 👋</h1>
+                    <h1 className={styles.textPresentation}>
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .typeString('Frontend')
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .typeString('Backend')
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .typeString('Fullstack developer here 👋')
+                                    .pauseFor(20000)
+                                    .start();
+                            }}
+                            options={{
+                                loop: true,
+                            }}
+                        />
+                    </h1>
                     <h3 className={styles.paragraphPresentation}>Hi, I&apos;m Fabrizzio Vilchez, just a passionated fullstack developer from Lima, Perú 🇵🇪 </h3>
                     <div className={styles.socialMedia}>
                         <Link href={"https://www.instagram.com/its_fabrizzio_ve/?hl=es"} className={styles.mediaIcons} target="_blank">
