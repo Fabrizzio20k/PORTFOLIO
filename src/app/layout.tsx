321b17075fbe94816c769296c9f338ba97be07e2
Fabrizzio20k <fabrizzio.vilchez.e@utec.edu.pe>
2023-12-26 13:39:58 -0500

    Add Metadata for page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,25 @@
 import { Navbar } from "@/components"
 import { Footer } from "@/components"
+import { Metadata } from 'next'
 import "@/styles/global.css"
+
+export const metadata: Metadata = {
+  title: 'Fabrizzio Vilchez | Full Stack Developer',
+  description: 'The personal website of Fabrizzio Vilchez, a full stack developer based in Lima, Perú.',
+  keywords: 'Fabrizzio Vilchez, Fabrizzio, Vilchez, Fabrizzio20k, Fabrizzio Vilchez Espinoza, Fabrizzio Desarrollador, Full Stack Developer',
+  icons: [
+    {
+      url: '/icon.ico',
+      href: '/icon.ico',
+      rel: 'icon',
+    },
+  ],
+  authors: [
+    {
+      name: 'Fabrizzio Vilchez',
+    },
+  ],
+}
 
 export default function RootLayout({
   children,
