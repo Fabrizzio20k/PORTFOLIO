ebefec61c4401ba8f1e2aad453362f68de254c68
Fabrizzio20k <fabrizzio.vilchez.e@utec.edu.pe>
2023-12-26 13:23:50 -0500

    Add styles for footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,30 @@
 import styles from "@/styles/Footer.module.css";
+import Link from "next/link";
+import { SiNextdotjs } from "react-icons/si";
+import { FaInstagram } from "react-icons/fa";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { FiGithub } from "react-icons/fi";
 
 function Footer() {
     return (
-        <footer className="footer">
-            <div className="container">
-                <span className="text-muted">Place sticky footer content here.</span>
+        <footer className={styles.Footer}>
+            <div className={styles.Container}>
+                <div className={styles.Copyright}>
+                    <h3 className={styles.Text}>
+                        Copyright &copy; 2023. Made with ❤ using <SiNextdotjs />
+                    </h3>
+                </div>
+                <div className={styles.Social}>
+                    <Link href={"https://www.instagram.com/its_fabrizzio_ve/?hl=es"} className={styles.MediaIcons} target="_blank">
+                        <FaInstagram />
+                    </Link>
+                    <Link href={"https://www.linkedin.com/in/fabrizzio20k/"} className={styles.MediaIcons} target="_blank">
+                        <AiOutlineLinkedin />
+                    </Link>
+                    <Link href={"https://github.com/Fabrizzio20k"} className={styles.MediaIcons} target="_blank">
+                        <FiGithub />
+                    </Link>
+                </div>
             </div>
         </footer>
     );
