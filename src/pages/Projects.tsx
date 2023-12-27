110375116e5edcb0baf9301accf13d8649d3e13a
Fabrizzio20k <fabrizzio.vilchez.e@utec.edu.pe>
2023-12-27 18:31:43 -0500

    Add Project Component

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,20 +1,12 @@
+import { Section } from "@/components";
+
 function Projects() {
     return (
-        <div>
-            <h1>Projects</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aut aperiam repellat! Enim ratione eveniet dolor qui incidunt nemo, quibusdam natus debitis tenetur dignissimos autem dolorem, eum, et temporibus at!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aut aperiam repellat! Enim ratione eveniet dolor qui incidunt nemo, quibusdam natus debitis tenetur dignissimos autem dolorem, eum, et temporibus at!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aut aperiam repellat! Enim ratione eveniet dolor qui incidunt nemo, quibusdam natus debitis tenetur dignissimos autem dolorem, eum, et temporibus at!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum optio facilis, omnis officia fugit ex, beatae cumque quo sit, dolorum veritatis perferendis esse dolores sint mollitia. Eligendi culpa rerum molestiae!
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam velit voluptates quisquam minima temporibus minus corrupti veritatis aliquid repellendus quaerat sed, alias autem nostrum ut quidem qui harum. Facere, fuga.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aut aperiam repellat! Enim ratione eveniet dolor qui incidunt nemo, quibusdam natus debitis tenetur dignissimos autem dolorem, eum, et temporibus at!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aut aperiam repellat! Enim ratione eveniet dolor qui incidunt nemo, quibusdam natus debitis tenetur dignissimos autem dolorem, eum, et temporibus at!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aut aperiam repellat! Enim ratione eveniet dolor qui incidunt nemo, quibusdam natus debitis tenetur dignissimos autem dolorem, eum, et temporibus at!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum optio facilis, omnis officia fugit ex, beatae cumque quo sit, dolorum veritatis perferendis esse dolores sint mollitia. Eligendi culpa rerum molestiae!
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam velit voluptates quisquam minima temporibus minus corrupti veritatis aliquid repellendus quaerat sed, alias autem nostrum ut quidem qui harum. Facere, fuga.
-            </p>
-        </div>
+        <Section color="white" flexDirection="column">
+            <div>
+                <h1>Projects</h1>
+            </div>
+        </Section>
     );
 }
 
